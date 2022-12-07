1e3b8d9fd3c67395ac03dc97a7b8a91cf2cc658c
wrijlarr <wrijlarr@amazon.com>
2022-12-07 11:04:23 -0500

    meme changes

--- memes/meme.js
+++ memes/meme.js
@@ -1,8 +1,11 @@
-{ 
+{
 }
+const CARD_DATA_KEY = "card-data";
+
 const addForm = document.querySelector("#addModal form");
 
 addForm.addEventListener("submit", handleAddSubmit);
+window.addEventListener("load", addAllCardsToUI)
 
 function handleAddSubmit(event){
     event.preventDefault();
@@ -12,6 +15,7 @@ function handleAddSubmit(event){
 
     const cardData = {imageUrl,topText, bottomText}
     addCardToUI(cardData);
+    addCardToDB(cardData);
     
     // Clear input fields
     addForm.reset();
@@ -35,14 +39,37 @@ function addCardToUI(cardData){
     </div>
   </div>
 </div>`;
+
+// Add data to image, title, and description
 cardCol.querySelector(".card-img-top").setAttribute("src", cardData.imageUrl);
 cardCol.querySelector(".card-img-top").setAttribute("alt", cardData.topText);  
 
 
-
+// add cardCol to the UI
 cardCol.querySelector(".card-title").textContent = cardData.topText;
 cardCol.querySelector(".card-text").textContent = cardData.bottomText;
 
 document.getElementById("cardContainer");
 cardContainer.append(cardCol);
+}
+
+function addCardToDB(cardData) {
+    let data = loadDataFromDB();
+    data.push(cardData);
+
+    //save to local storage
+    localStorage.setItem(CARD_DATA_KEY, JSON.stringify(data))
+
+}
+function addAllCardsToUI(evt) {
+    let data = loadDataFromDB();
+    data.forEach((cardData) => addCardToUI(cardData));
+}
+
+function loadDataFromDB(){
+    let data = JSON.parse(localStorage.getItem(CARD_DATA_KEY));
+    if (!data) {
+        data = [];
+    }
+    return data;
 }
