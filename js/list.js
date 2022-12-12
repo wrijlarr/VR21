03778b2231a3a26895032726d28c51dfc82bcc5a
wrijlarr <wrijlarr@amazon.com>
2022-12-12 12:31:54 -0500

    new changes

--- js/list.js
+++ js/list.js
@@ -1,34 +1,33 @@
 const CARD_DATA_KEY = "card-data";
 const CARD_TITLE_ATTRIBUTE = "data-list-title";
 
-
 const addForm = document.querySelector("#addModal form");
 addForm.addEventListener("submit", handleAddSubmit);
-window.addEventListener("load", addAllCardsToUI)
+window.addEventListener("load", addAllCardsToUI);
 
-function handleAddSubmit(event){
-    event.preventDefault();
-    const imageUrl = addForm.elements.imageUrl.value;
-    const title = addForm.elements.title.value;
-    const description = addForm.elements.description.value;
+function handleAddSubmit(event) {
+  event.preventDefault();
+  const imageUrl = addForm.elements.imageUrl.value;
+  const title = addForm.elements.title.value;
+  const description = addForm.elements.description.value;
 
-    const cardData = {imageUrl,title,description}
-    addCardToUI(cardData);
-    addCardToDB(cardData);
-    
-    // Clear input fields
-    addForm.reset();
-    addForm.removeAttribute("data-cardId");
+  const cardData = { imageUrl, title, description };
+  addCardToUI(cardData);
+  addCardToDB(cardData);
 
-    const closeBtn = document.querySelector('[data-bs-dismiss="modal"]');
-    closeBtn.click();
+  // Clear input fields
+  addForm.reset();
+  addForm.removeAttribute("data-cardId");
+
+  const closeBtn = document.querySelector('[data-bs-dismiss="modal"]');
+  closeBtn.click();
 }
 
-function addCardToUI(cardData){
-    // create template with card in it
-    const cardCol = document.createElement('div')
-    cardCol.classList.add("col");
-    cardCol.innerHTML = `
+function addCardToUI(cardData) {
+  // create template with card in it
+  const cardCol = document.createElement("div");
+  cardCol.classList.add("col");
+  cardCol.innerHTML = `
     <div class="card" style="width: 18rem;">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
@@ -44,59 +43,56 @@ function addCardToUI(cardData){
   </div>
 </div>`;
 
-// Add data to image, title, and description
-cardCol.querySelector(".card-img-top").setAttribute("src", cardData.imageUrl);
-cardCol.querySelector(".card-img-top").setAttribute("alt", cardData.title);  
+  // Add data to image, title, and description
+  cardCol.querySelector(".card-img-top").setAttribute("src", cardData.imageUrl);
+  cardCol.querySelector(".card-img-top").setAttribute("alt", cardData.title);
 
-cardCol.querySelector(".card-title").textContent = cardData.title;
-cardCol.querySelector(".card-text").textContent = cardData.description;
+  cardCol.querySelector(".card-title").textContent = cardData.title;
+  cardCol.querySelector(".card-text").textContent = cardData.description;
 
-// Enable delete funtionality
-const deleteBtn = cardCol.querySelector(".btn-danger");
-deleteBtn.addEventListener("click", deleteCard);
-cardCol.setAttribute(CARD_TITLE_ATTRIBUTE, cardData.title);
+  // Enable delete funtionality
+  const deleteBtn = cardCol.querySelector(".btn-danger");
+  deleteBtn.addEventListener("click", deleteCard);
+  cardCol.setAttribute(CARD_TITLE_ATTRIBUTE, cardData.title);
 
-
-//Add Cardcol to UI
-document.getElementById("cardContainer");
-cardContainer.append(cardCol);
+  //Add Cardcol to UI
+  document.getElementById("cardContainer");
+  cardContainer.append(cardCol);
 }
 
 function addCardToDB(cardData) {
-    let data = loadDataFromDB();
-    data.push(cardData);
+  let data = loadDataFromDB();
+  data.push(cardData);
 
-saveDataToDB(data);
+  saveDataToDB(data);
 }
 
 function addAllCardsToUI(evt) {
-    let data = loadDataFromDB();
-    data.forEach((cardData) => addCardToUI(cardData));
+  let data = loadDataFromDB();
+  data.forEach((cardData) => addCardToUI(cardData));
 }
 
-function loadDataFromDB(){
-    let data = JSON.parse(localStorage.getItem(CARD_DATA_KEY));
-    if (!data) {
-        data = [];
-    }
-    return data;
+function loadDataFromDB() {
+  let data = JSON.parse(localStorage.getItem(CARD_DATA_KEY));
+  if (!data) {
+    data = [];
+  }
+  return data;
 }
 
-function deleteCard(evt){
-    // The button that gets clicked
-    const deleteBtn = evt.target;
-    
-    // Select button that contains card
-    const cardCol = deleteBtn.closest(".col");
-    const titleToDelete = cardCol.getAttribute(CARD_TITLE_ATTRIBUTE)
-    let data = loadDataFromDB();
-    data.filter((cardData) => cardData.title !== titleToDelete);
-    saveDataToDB(data);
-    cardCol.remove();
+function deleteCard(evt) {
+  // The button that gets clicked
+  const deleteBtn = evt.target;
+
+  // Select button that contains card
+  const cardCol = deleteBtn.closest(".col");
+  const titleToDelete = cardCol.getAttribute(CARD_TITLE_ATTRIBUTE);
+  let data = loadDataFromDB();
+  data.filter((cardData) => cardData.title !== titleToDelete);
+  saveDataToDB(data);
+  cardCol.remove();
 }
 
-function updateCard(evt) {
-
-}
+function updateCard(evt) {}
 const card = evt.target.closest(".card");
 const cardID = card.getAttribute("data-cardID");
