c1316a114def792edeada96bcc4a787baf73debd
wrijlarr <wrijlarr@amazon.com>
2022-12-13 13:59:06 -0500

    new changes

---
+++ typehead/index.js
@@ -0,0 +1,43 @@
+const arrayOfObjects = [
+{name: "Fox", link: "foxnews.com"},
+{name: "Fandango", link: "fandango.com"}, 
+{name: "CNN", link: "cnn.com"},
+
+];
+
+const SUGGESTIONS_ID = "suggestions";
+
+const searchInput = document.getElementById("search");
+searchInput.addEventListener("keyup", handleKeyUp);
+
+function handleKeyUp (e) {
+    const userInput = searchInput.value.toLowerCase();
+    // words that contain the text the user has typed
+    const suggestions = arrayOfObjects.filter((result) => 
+    result.name.toLowerCase().includes(userInput)
+    );
+    showSuggestions(suggestions);
+}
+
+function showSuggestions(suggestions) {
+        const ul = document.getElementById(SUGGESTIONS_ID);
+
+        clearSuggestions();
+
+        suggestions.forEach((result) => {
+            const li = document.createElement("li");
+            li.innerHTML= `
+            <a href=":=${result.link}">${result.name}</a>
+        `;
+        ul.append(li);    
+        });
+    }
+function clearSuggestions() {
+        const ul = document.getElementById(SUGGESTIONS_ID);
+        while(ul.firstElementChild) {
+            ul.firstElementChild.remove();
+        }
+    }
+
+
+
